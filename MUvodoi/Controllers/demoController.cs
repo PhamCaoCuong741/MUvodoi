ddfec60d40cc9ad3f4ccf457a8cbff005035f81d
CUONG PHAM CAO <1821050741@student.humg.edu.vn>
2021-09-14 06:59:35 +0700

    r

--- MUvodoi/Controllers/demoController.cs
+++ MUvodoi/Controllers/demoController.cs
@@ -10,7 +10,6 @@ namespace BaiTap2.Controllers
     public class DemoBT2Controller : Controller
     {
         GiaiPhuongTrinh gpt = new GiaiPhuongTrinh();
-        // GET: DemoBT2
         public ActionResult GPT()
         {
             return View();
@@ -18,13 +17,8 @@ namespace BaiTap2.Controllers
         [HttpPost]
         public ActionResult GPT(string soA, string soB)
         {
-            //nhan 2 gia tri gui len tu client
-            //chuyen oi sang kieu doubl
-            //2 giatri nay làm tham so dau vao ho phon thuc gii phuong trinh
             double so1 = Convert.ToDouble(soA);
             double so2 = Convert.ToDouble(soB);
-            //Khai bao bien x
-            // ke thua phuong thuc giai pt
             double ketqua = gpt.GPT(so1, so2);
             ViewBag.Giaipt = ketqua;
             return View();
